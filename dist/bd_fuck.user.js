ca0e3afab1065f820a764a54d20676a9c8493543
axetroy <troy450409405@gmail.com>
2016-11-10 00:45:44 +0800

    update meta

--- dist/bd_fuck.user.js
+++ dist/bd_fuck.user.js
@@ -1,19 +1,23 @@
 // ==UserScript==
-// @name    remove_baidu_redirect
-// @author  burningall
-// @description test
-// @version     2016.11.10
-// @include     *www.baidu.com*
-// @grant       GM_xmlhttpRequest
-// @connect     *
-// @run-at      document-start
-// @compatible  chrome  两个字，破费
-// @compatible  firefox  两个字，破费
-// @license     The MIT License (MIT); http://opensource.org/licenses/MIT
-// @supportURL      http://www.burningall.com
-// @contributionURL troy450409405@gmail.com|alipay.com
-// @namespace https://greasyfork.org/zh-CN/users/3400-axetroy
+// @name              remove the jump link in BAIDU (typescript)
+// @author            axetroy
+// @collaborator      axetroy
+// @description       去除百度搜索跳转链接
+// @version           2016.11.10
+// @grant             GM_xmlhttpRequest
+// @include           *www.baidu.com*
+// @connect           *
+// @compatible        chrome  完美运行
+// @compatible        firefox  完美运行
+// @supportURL        http://www.burningall.com
+// @run-at            document-start
+// @contributionURL   troy450409405@gmail.com|alipay.com
+// @namespace         https://greasyfork.org/zh-CN/users/3400-axetroy
+// @license           The MIT License (MIT); http://opensource.org/licenses/MIT
 // ==/UserScript==
+
+// Github源码:https://github.com/axetroy/bd-fuck
+
 
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
