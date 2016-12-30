bd1505d0b87716ea7fec788c6f5a28fb6db35d56
axetroy <troy450409405@gmail.com>
2016-12-30 12:23:44 +0800

    fix: fix a bug about class Query

--- lib/query.ts
+++ lib/query.ts
@@ -5,29 +5,29 @@
 export default class Query {
   object: any = {};
 
-  constructor(public queryStr: String) {
+  constructor(public queryStr: String = '') {
     this.object = this.toObject(queryStr.replace(/^\?+/, ''));
   }
 
-  toObject(queryStr: String) {
+  private toObject(queryStr: String) {
     let obj = {};
     queryStr.split('&').forEach((item)=> {
       let arr = item.split('=') || [];
       let key = arr[0] || '';
-      obj[key] = arr[1] || '';
+      key && (obj[key] = arr[1] || '');
     });
     return obj;
   }
 
   public toString(): String {
-    let arr = [];
+    let arr: string[] = [];
     for (let key in this.object) {
       if (this.object.hasOwnProperty(key)) {
         let value = this.object[key];
         arr.push(key + '=' + value);
       }
     }
-    return '?' + arr.join('&');
+    return arr.length ? '?' + arr.join('&') : '';
   }
 
 }
