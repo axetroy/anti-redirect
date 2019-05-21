19c7439f0b78ae06bf79b04e6eb528cfe7da26c9
axetroy <troy450409405@gmail.com>
2019-05-21 12:33:31 +0800

    fix Google play 部分链接没有被去除的问题. close #66

--- src/sites/google.play.ts
+++ src/sites/google.play.ts
@@ -1,8 +1,39 @@
 import { IProvider } from "../provider";
-import { antiRedirect } from "../utils";
+import { antiRedirect, Marker } from "../utils";
 export class GooglePlayProvider implements IProvider {
-  public test = /google\.com\/url\?q=(.*)/;
+  public test(aElement: HTMLAnchorElement) {
+    if (/google\.com\/url\?q=(.*)/.test(aElement.href)) {
+      return true;
+    } else if (/^\/store\/apps\/details/.test(location.pathname)) {
+      return true;
+    }
+
+    return false;
+  }
   public resolve(aElement: HTMLAnchorElement) {
     antiRedirect(aElement, new URL(aElement.href).searchParams.get("q"));
+
+    // 移除开发者栏目下的重定向
+    const eles = [].slice.call(document.querySelectorAll("a.hrTbp"));
+
+    for (const ele of eles) {
+      if (!ele.href) {
+        continue;
+      }
+      if (ele.getAttribute(Marker.RedirectStatusDone)) {
+        continue;
+      }
+
+      ele.setAttribute(Marker.RedirectStatusDone, ele.href);
+      ele.setAttribute("target", "_blank");
+
+      ele.addEventListener(
+        "click",
+        event => {
+          event.stopPropagation();
+        },
+        true
+      );
+    }
   }
 }
