5cb1da06d1d6337c922363a539c50984ed17b556
axetroy <axetroy.dev@gmail.com>
2020-02-26 13:20:41 +0800

    fix: 适配 Google 的新追踪策略. close #176

--- src/sites/google.ts
+++ src/sites/google.ts
@@ -1,15 +1,10 @@
 import { IProvider } from "../provider";
-import { antiRedirect } from "../utils";
 export class GoogleProvider implements IProvider {
   public test = true;
   public resolve(aElement: HTMLAnchorElement) {
-    // 移除多余的事件
-    if (aElement.getAttribute("onmousedown")) {
-      aElement.removeAttribute("onmousedown");
-    }
-    if (aElement.getAttribute("data-href")) {
-      const realUrl: string = aElement.getAttribute("data-href");
-      antiRedirect(aElement, realUrl);
+    // 移除追踪
+    if (aElement.getAttribute("ping")) {
+      aElement.removeAttribute("ping");
     }
   }
 }
