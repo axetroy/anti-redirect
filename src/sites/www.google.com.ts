b4e34e6e814c5856ddb252a0b13fe1a5aeddf4ad
axetroy <axetroy.dev@gmail.com>
2020-12-25 21:09:23 +0800

    update google

--- src/sites/www.google.com.ts
+++ src/sites/www.google.com.ts
@@ -1,10 +1,23 @@
 import { IProvider } from "@/provider";
+import { antiRedirect } from "../utils";
+
 export class GoogleProvider implements IProvider {
   public test = true;
   public resolve(aElement: HTMLAnchorElement) {
     // 移除追踪
     if (aElement.getAttribute("ping")) {
       aElement.removeAttribute("ping");
+    }
+
+    // 移除多余的事件
+    if (aElement.getAttribute("onmousedown")) {
+      aElement.removeAttribute("onmousedown");
+    }
+
+    // 尝试去除重定向
+    if (aElement.getAttribute("data-href")) {
+      const realUrl: string = aElement.getAttribute("data-href");
+      antiRedirect(aElement, realUrl);
     }
   }
 }
