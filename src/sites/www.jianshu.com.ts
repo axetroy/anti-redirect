b5cb3c7eca7ad8fb79ec9c678710e8a205860db4
axetroy <axetroy.dev@gmail.com>
2021-01-20 09:26:53 +0800

    fix: 修复简书有些链接没有去除重定向 close #366

--- src/sites/www.jianshu.com.ts
+++ src/sites/www.jianshu.com.ts
@@ -5,8 +5,9 @@ export class JianShuProvider implements IProvider {
   public test = (aElement: HTMLAnchorElement) => {
     const isLink1 = /links\.jianshu\.com\/go/.test(aElement.href)
     const isLink2 = /link\.jianshu\.com(\/)?\?t=/.test(aElement.href)
+    const isLink3 = /jianshu\.com\/go-wild\/?\?(.*)url=/.test(aElement.href)
 
-    if (isLink1 || isLink2) {
+    if (isLink1 || isLink2 || isLink3) {
       return true
     }
 
@@ -14,6 +15,6 @@ export class JianShuProvider implements IProvider {
   }
   public resolve(aElement: HTMLAnchorElement) {
     const search = new URL(aElement.href).searchParams
-    antiRedirect(aElement, search.get('to') || search.get('t'))
+    antiRedirect(aElement, search.get('to') || search.get('t') || search.get('url'))
   }
 }
