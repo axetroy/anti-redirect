a265fa4f0c1e8575c428c657c12c0abde90fbad1
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2021-08-31 16:07:50 +0800

    fix: keep cached page link

--- src/sites/cn.bing.com.ts
+++ src/sites/cn.bing.com.ts
@@ -4,14 +4,16 @@ export class CnBingProvider implements IProvider {
   public test = true;
   private container: HTMLElement;
   public resolve(aElement: HTMLAnchorElement) {
-    this.container = document.querySelector("#b_results")
-    if (this.container && this.container.contains(aElement)) {
-      aElement.removeAttribute('onclick')
-      // Replace with a clone to remove all event listeners
-      aElement.parentElement.replaceChild(
-        aElement.cloneNode(true),
-        aElement,
-      );
+    if (aElement.innerText && aElement.innerText !== 'Cached') {
+      this.container = document.querySelector("#b_results")
+      if (this.container && this.container.contains(aElement)) {
+        aElement.removeAttribute('onclick')
+        // Replace with a clone to remove all event listeners
+        aElement.parentElement.replaceChild(
+          aElement.cloneNode(true),
+          aElement,
+        );
+      }
     }
   }
 }
