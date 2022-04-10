0efa80fde242eeaf092cc9d69b547dd3a8e78397
Axetroy <axetroy.dev@gmail.com>
2022-04-10 15:46:13 +0800

    fix: 提升百度的稳定性

--- src/sites/www.baidu.com.ts
+++ src/sites/www.baidu.com.ts
@@ -4,19 +4,22 @@ import {
   getRedirect,
   increaseRedirect,
   decreaseRedirect,
-  antiRedirect
+  antiRedirect,
 } from "@/utils";
+import pRetry from "p-retry";
 
 export class BaiduProvider implements IProvider {
   public test = /www\.baidu\.com\/link\?url=/;
   public resolve(aElement: HTMLAnchorElement) {
+    console.log(aElement);
     if (getRedirect(aElement) <= 2 && this.test.test(aElement.href)) {
       increaseRedirect(aElement);
-      this.handlerOneElement(aElement)
-        .then(res => {
+
+      pRetry(() => this.handlerOneElement(aElement), { retries: 3 })
+        .then((res) => {
           decreaseRedirect(aElement);
         })
-        .catch(err => {
+        .catch((err) => {
           decreaseRedirect(aElement);
         });
     }
@@ -24,17 +27,20 @@ export class BaiduProvider implements IProvider {
 
   private async handlerOneElement(aElement: HTMLAnchorElement): Promise<any> {
     try {
-      const res: Response$ = await http.request({
+      const res = await http.request({
         url: aElement.href,
         method: "GET",
-        anonymous: true
+        anonymous: true,
       });
+
       if (res.finalUrl) {
         antiRedirect(aElement, res.finalUrl);
       }
+
       return res;
     } catch (err) {
       console.error(err);
+      return Promise.reject(new Error(`[http]: ${aElement.href} fail`));
     }
   }
 }
