c64381e9f6bc7fee108e3daf3af5743ca5b0278c
Axetroy <axetroy.dev@gmail.com>
2022-04-22 00:20:34 +0800

    fix: google

--- src/sites/www.google.com.ts
+++ src/sites/www.google.com.ts
@@ -4,9 +4,13 @@ import { antiRedirect } from "../utils";
 export class GoogleProvider implements IProvider {
   public test = true;
   public resolve(aElement: HTMLAnchorElement) {
+    const traceProperties = ["ping", "data-jsarwt", "data-usg", "data-ved"];
+
     // 移除追踪
-    if (aElement.getAttribute("ping")) {
-      aElement.removeAttribute("ping");
+    for (const property of traceProperties) {
+      if (aElement.getAttribute(property)) {
+        aElement.removeAttribute(property);
+      }
     }
 
     // 移除多余的事件
@@ -18,6 +22,12 @@ export class GoogleProvider implements IProvider {
     if (aElement.getAttribute("data-href")) {
       const realUrl: string = aElement.getAttribute("data-href");
       antiRedirect(aElement, realUrl);
+    }
+
+    const url = new URL(aElement.href);
+
+    if (url.searchParams.get("url")) {
+      antiRedirect(aElement, url.searchParams.get("url"));
     }
   }
 }
